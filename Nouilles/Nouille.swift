eb34fb677ac64e09a4ad0aeaeccc9028c7b8f6a2
Denis Ricard <d.ricard@me.com>
2016-08-22 16:38:53 -0400

    feat: add basic data models (Nouille et Nouilles)

---
+++ Nouilles/Nouille.swift
@@ -0,0 +1,22 @@
+//
+//  Nouille.swift
+//  Nouilles
+//
+//  Created by Denis Ricard on 2016-08-22.
+//  Copyright © 2016 Hexaedre. All rights reserved.
+//
+
+import UIKit
+
+class Nouille: NSObject {
+    
+    // MARK: - properties
+    let name: String
+    let time: Float
+    
+    init(name: String, time: Float) {
+        self.name = name
+        self.time = time
+    }
+    
+}
---
+++ Nouilles/Nouilles.swift
@@ -0,0 +1,17 @@
+//
+//  Nouilles.swift
+//  Nouilles
+//
+//  Created by Denis Ricard on 2016-08-22.
+//  Copyright © 2016 Hexaedre. All rights reserved.
+//
+
+import UIKit
+
+class Nouilles: NSObject {
+
+    // MARK: - properties
+    let listeDeNouilles = [Nouille]()
+    
+    
+}
