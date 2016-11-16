ce7ac10fe283c7fb0e75ce381214f61a943baf1e
Denis Ricard <d.ricard@me.com>
2016-11-16 09:50:51 -0500

    feat: add AddNoodleVC and storyboard VC

---
+++ Nouilles/AddNoodleVC.swift
@@ -0,0 +1,35 @@
+//
+//  AddNoodleVC.swift
+//  Nouilles
+//
+//  Created by Denis Ricard on 2016-11-16.
+//  Copyright © 2016 Hexaedre. All rights reserved.
+//
+
+import UIKit
+
+class AddNoodleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
