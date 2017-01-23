1184beac5bd48bb88ba50ac0e9976ca028695ade
Denis Ricard <d.ricard@me.com>
2017-01-23 16:11:06 -0500

    refactor: cleanup app delegate

--- Nouilles/AppDelegate.swift
+++ Nouilles/AppDelegate.swift
@@ -11,39 +11,30 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-   
-   var window: UIWindow?
-   
-   // Instanciate the CoreDataStack
-   lazy var coreDataStack = CoreDataStack(modelName: "Model")
-   
-   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-      
-      guard let navController = window?.rootViewController as? UINavigationController, let viewController = navController.topViewController as? ListeDeNouillesVC else { return true }
-      
-      // pass the Core Data Context to the first View Controller (dependency injection)
-      viewController.managedContext = coreDataStack.managedContext
-      
-      return true
-   }
-   
-   func applicationWillResignActive(_ application: UIApplication) {
-   }
-   
-   func applicationDidEnterBackground(_ application: UIApplication) {
-      coreDataStack.saveContext()
-   }
-   
-   func applicationWillEnterForeground(_ application: UIApplication) {
-   }
-   
-   func applicationDidBecomeActive(_ application: UIApplication) {
-   }
-   
-   func applicationWillTerminate(_ application: UIApplication) {
-      coreDataStack.saveContext()
-   }
-   
-   
+    
+    var window: UIWindow?
+    
+    // Instanciate the CoreDataStack
+    lazy var coreDataStack = CoreDataStack(modelName: "Model")
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        // get a reference to the first view controller...
+        guard let navController = window?.rootViewController as? UINavigationController, let viewController = navController.topViewController as? ListeDeNouillesVC else { return true }
+        
+        // ... and pass the Core Data Context to it (dependency injection pattern)
+        viewController.managedContext = coreDataStack.managedContext
+        
+        return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        coreDataStack.saveContext()
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        coreDataStack.saveContext()
+    }
+    
 }
 
