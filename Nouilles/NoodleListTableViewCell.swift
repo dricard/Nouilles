eebea9f19446443588833cc5222c9a3b3f3d2168
Denis Ricard <d.ricard@me.com>
2017-02-04 20:30:42 -0500

    refactor: remove unused code from NoodleListTableViewCell

--- Nouilles/NoodleListTableViewCell.swift
+++ Nouilles/NoodleListTableViewCell.swift
@@ -27,16 +27,4 @@ class NoodleListTableViewCell: UITableViewCell {
     @IBOutlet weak var onHandIndicatorView: UIImageView!
     @IBOutlet weak var timerView: TimerView!
     
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
-    }
-    
 }
