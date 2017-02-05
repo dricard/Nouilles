00d732e90aa247f02df1f9677f8424918315a805
Denis Ricard <d.ricard@me.com>
2017-02-04 20:32:47 -0500

    refactor: remove unused code from NumberTableViewCell

--- Nouilles/NumberTableViewCell.swift
+++ Nouilles/NumberTableViewCell.swift
@@ -16,18 +16,4 @@ class NumberTableViewCell: UITableViewCell {
    @IBOutlet weak var label: UILabel!
    @IBOutlet weak var textField: UITextField!
    
-   
-   // MARK: - Life Cycle
-   
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
