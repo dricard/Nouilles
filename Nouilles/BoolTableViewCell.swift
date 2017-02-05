6b96041c6018f792889e9d209d7f3e221b8b500a
Denis Ricard <d.ricard@me.com>
2017-02-04 20:33:25 -0500

    refactor: remove unused code from BoolTableViewCell

--- Nouilles/BoolTableViewCell.swift
+++ Nouilles/BoolTableViewCell.swift
@@ -16,18 +16,4 @@ class BoolTableViewCell: UITableViewCell {
    @IBOutlet weak var label: UILabel!
    @IBOutlet weak var boolStateSwitch: UISwitch!
    
-   
-   // MARK: - Life Cycle
-   
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
